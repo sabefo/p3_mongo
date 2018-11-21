3490b55a04e8d52bd5aabc92e57407609498d19d
Santiago Bermúdez <sabefo@eduroam098006.eduroam.ucm.es>
2018-11-21 17:56:53 +0100

    primer commit

---
+++ consultas.js
@@ -0,0 +1,53 @@
+/*
+* CABECERA AQUI
+*/
+
+
+
+/* AGGREGATION PIPELINE */
+// 1.- 
+function agg1(){
+	/* */
+}
+
+// 2.- 
+function agg2(){
+  /* */
+}
+  
+// 3.- 
+function agg3(){
+  /* */
+}  
+  
+  
+// 4.- 
+function agg4(){
+  /* */
+}
+
+
+
+  
+/* MAPREDUCE */  
+  
+// 1.- 
+function mr1(){
+	/* */
+}
+
+// 2.- 
+function mr2(){
+	/* */
+}
+
+// 3.- 
+function mr3(){
+	/* */
+}
+
+// 4.- 
+function mr4(){
+	/* */
+}
+
---
+++ consultas.py
@@ -0,0 +1,51 @@
+# 
+# CABECERA AQUI
+#
+
+# -*- coding: utf-8 -*-
+
+
+# 1. Fecha y título de las primeras 'n' peliculas vistas por el usuario 'user_id'
+# usuario_peliculas( 'fernandonoguera', 3 )
+def usuario_peliculas(user_id, n):
+    pass
+    
+   
+# 2. _id, nombre y apellidos de los primeros 'n' usuarios a los que les gusten 
+# varios tipos de película 'gustos' a la vez
+# usuarios_gustos(  ['terror', 'comedia'], 5  )
+def usuarios_gustos(gustos, n):
+    pass
+
+  
+# 3. _id de usuario de sexo 'sexo' y con una edad entre 'edad_min' e 'edad_max' incluidas
+# usuario_sexo_edad('M', 50, 80)
+def usuario_sexo_edad( sexo, edad_min, edad_max ):
+    pass
+
+    
+# 4. Nombre, apellido1 y apellido2 de los usuarios cuyos apellidos coinciden,
+#    ordenado por edad ascendente
+# usuarios_apellidos()
+def usuarios_apellidos():
+    pass
+    
+    
+# 5.- Titulo de peliculas cuyo director empiece con un 'prefijo' dado
+# pelicula_prefijo( 'Yol' )
+def pelicula_prefijo( prefijo ):
+    pass
+    
+
+# 6.- _id de usuarios con exactamente 'n' gustos, ordenados por edad descendente
+# usuarios_gustos_numero( 6 )
+def usuarios_gustos_numero(n):
+    pass
+    
+    
+# 7.- usuarios que vieron pelicula la pelicula 'id_pelicula' en un periodo 
+#     concreto 'inicio' - 'fin'
+# usuarios_vieron_pelicula( '583ef650323e9572e2812680', '2015-01-01', '2016-12-31' )
+def usuarios_vieron_pelicula(id_pelicula, inicio, fin):
+    pass
+
