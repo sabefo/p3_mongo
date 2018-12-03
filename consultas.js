d3f5b3c2ae768c05c1cc0ab80f2eabec9d7d7233
Santiago Bermúdez <sabefo@eduroam109140.eduroam.ucm.es>
2018-12-03 19:20:36 +0100

    conexion a base de datos y return en funciones

--- consultas.js
+++ consultas.js
@@ -26,75 +26,75 @@
 // "fecha" :"1978-12-27" } ],
 // }
 
+conn = new Mongo();
+db = conn.getDB('sgdi_pr3')
+
 /* AGGREGATION PIPELINE */
 // 1.- Listado de paıs-numero de pelıculas rodadas en el, ordenado por numero de pelıculas descendente y en caso de empate por nombre paıs ascendente
 function agg1() {
-db.peliculas
-  .aggregate([
-    { $unwind: "$pais" },
-    { $group: { _id: "$pais", count: { $sum: 1 } } },
-    { $sort: { count: -1, _id: 1 } }
-  ])
+  return db.peliculas
+    .aggregate([
+      { $unwind: "$pais" },
+      { $group: { _id: "$pais", count: { $sum: 1 } } },
+      { $sort: { count: -1, _id: 1 } }
+    ])
 }
 
 // 2.- Listado de los 3 tipos de pelıcula mas populares entre los usuarios de los ’Emiratos Árabes Unidos’, ordenado de mayor a menor numero de usuarios que les gusta. En caso de empate a numero de usuarios, se usa el tipo de pelıcula de manera ascendente.
 function agg2() {
-db.usuarios
-db.usuarios
-  .aggregate([
-    { $match: { "direccion.pais": "Emiratos Árabes Unidos" } },
-    { $unwind: "$gustos" },
-    { $project: { _id: 0, gusto: "$gustos" } },
-    { $group: { _id: "$gusto", count: { $sum: 1 } } },
-    { $sort: { count: -1, _id: 1 } },
-    { $limit: 3 }
-  ])
+  return db.usuarios
+    .aggregate([
+      { $match: { "direccion.pais": "Emiratos Árabes Unidos" } },
+      { $unwind: "$gustos" },
+      { $project: { _id: 0, gusto: "$gustos" } },
+      { $group: { _id: "$gusto", count: { $sum: 1 } } },
+      { $sort: { count: -1, _id: 1 } },
+      { $limit: 3 }
+    ])
 }
 
 // 3.- Listado de paıs-(edad mınima, edad-máxima, edad media) teniendo en cuenta unicamente los usuarios mayores de edad, es decir, con mas de 17 años. Los paıses con menos de 3 usuarios mayores de edad no deben aparecer en el resultado.
 function agg3() {
-db.usuarios
-  .aggregate([
-    { $match: { "edad": { $gt: 17 } } },
-    { $project: { _id: 0, edad: "$edad", pais: "$direccion.pais" } },
-    { $group: { _id: "$pais", min: { $min: "$edad" }, max: { $max: "$edad" }, avg: { $avg: "$edad" }, count: { $sum: 1 } } },
-    { $sort: { count: -1, _id: 1 } },
-  ])
+  return db.usuarios
+    .aggregate([
+      { $match: { "edad": { $gt: 17 } } },
+      { $project: { _id: 0, edad: "$edad", pais: "$direccion.pais" } },
+      { $group: { _id: "$pais", min: { $min: "$edad" }, max: { $max: "$edad" }, avg: { $avg: "$edad" }, count: { $sum: 1 } } },
+      { $match: {"count": { $gte : 3}}},
+      { $sort: { count: -1, _id: 1 } },
+    ])
 }
 
 // 4.- Listado de tıtulo pelıcula-numero de visualizaciones de las 10 pelıculas mas vistas, ordenado por numero descencente de visualizaciones. En caso de empate, romper por tıtulo de pelıcula ascendente.
 function agg4() {
-db.usuarios
-  .aggregate([
-    { $unwind: "$visualizaciones" },
-    { $project: { _id: 0, "visualizaciones.titulo": 1 } },
-    { $group: { _id: "$visualizaciones.titulo", count: { $sum: 1 } } },
-    { $sort: { count: -1, _id: 1 } },
-    { $limit: 10 }
-  ])
+  return db.usuarios
+    .aggregate([
+      { $unwind: "$visualizaciones" },
+      { $project: { _id: 0, "visualizaciones.titulo": 1 } },
+      { $group: { _id: "$visualizaciones.titulo", count: { $sum: 1 } } },
+      { $sort: { count: -1, _id: 1 } },
+      { $limit: 10 }
+    ])
 }
-
-
-
 
 /* MAPREDUCE */  
   
-// 1.-
+// 1.- Listado de paıs-numero de pelıculas rodadas en el.
 function mr1() {
   /* */
 }
 
-// 2.-
+// 2.- Listado de rango de edad -numero de usuarios. Los rangos de edad son periodos de 10 años: [0, 10), [10, 20), [20, 30), etc. Si no hay ningun usuario con edad en un rango concreto dicho rango no deberia aparecer en la salida.
 function mr2() {
   /* */
 }
 
-// 3.-
+// 3.- Listado de pais-(edad mınima, edad-maxima, edad media) teniendo en cuenta unicamente los usuarios con mas de 17 años.
 function mr3() {
   /* */
 }
 
-// 4.- 
+// 4.- Listado de año-numero de visualizaciones veraniegas, donde una @visualizacion veraniega es aquella que se ha producido en los meses de junio, julio o agosto.
 function mr4() {
   /* */
 }
